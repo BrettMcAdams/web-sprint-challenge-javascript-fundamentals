2a345071dea016dac03e1c5836a082d258eb42cc
BrettMcAdams96@gmail.com <brettmcadams96@gmail.com>
2020-07-24 12:32:47 -0400

    finished all sprint required tasks

--- challenges/closure.js
+++ challenges/closure.js
@@ -14,11 +14,22 @@ function myFunction() {
   };
   nestedFunction();
 }
-myFunction();
+// myFunction();
 
 // Explanation: 
 
+// nestedFunction can access the 'internal' variable because, while it is not inside the same function, it's still inside the scope of 'myFunction' and therefore can be accessed by it.
 
 /* Task 2: Counter */
 
 /* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+
+function summation(n) {
+  let sum = 0;
+  for (let i = 1; i <= n; i++) {
+    sum += i;
+  }
+  return sum;
+}
+
+// console.log(summation(4))
